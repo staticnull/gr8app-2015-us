c53bc776abcf2f8fa6e26eabf5644f9fef9bc9de
Soeren Berg Glasius <soeren@glasius.dk>
2015-03-23 10:25:55 -0700

    Ticket prices added

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -8,10 +8,11 @@
  * Controller of the gr8app2015App
  */
 angular.module('gr8conf2015')
-  .controller('VenueCtrl', function ($scope) {
+  .controller('RegistrationCtrl', ['$scope','CONFERENCE', function ($scope, CONFERENCE) {
       //$scope.prices = [
       //  {name: 'Regular', university: {dkk: 3275, euro: 435}, conference: {dkk: 4900, euro: 655}},
       //  {name: 'Early Bird', university: {dkk: 2175, euro: 290}, conference: {dkk: 3225, euro: 430 }},
       //  {name: 'Door Buster', }
       //]
-  });
+    $scope.registrationUrl=CONFERENCE.registrationUrl;
+  }]);
